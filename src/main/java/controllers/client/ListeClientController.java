edfac251d9b09cdec02ab00c627e03ac84df0f5d
AlexandreSama <alexandrehainy12@gmail.com>
2025-03-20 16:43:52 +0100

    Je commence a bosser sur la liste des clients. Mais je me demande si je dois pas modifier mes classes mères mdrr

--- src/main/java/controllers/client/ListeClientController.java
+++ src/main/java/controllers/client/ListeClientController.java
@@ -1,14 +1,30 @@
 package controllers.client;
 
 import controllers.ICommand;
+import controllers.user.UserForm;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
 /**.
  * Controller servant pour la page de liste de client
  * {@inheritDoc}
  */
 public class ListeClientController implements ICommand {
+
+    private final Connection connection;
+
+    private static final Logger LOGGER =
+            Logger.getLogger(ListeClientController.class.getName());
+
+    public ListeClientController(final Connection connection) {
+        this.connection = connection;
+    }
 
     /**.
      * Méthode d'éxécution du controller
@@ -20,6 +36,23 @@ public class ListeClientController implements ICommand {
     public String execute(final HttpServletRequest request,
                           final HttpServletResponse response)
             throws Exception {
-        return "client/listeClient.jsp";
+            try{
+                String sql = "SELECT * FROM client";
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                if(rs.next()){
+
+                }else{
+                    request.setAttribute("error",
+                            "Erreur, vous n'avez aucun client a votre actif");
+                    LOGGER.info("Aucun client trouvé");
+                    return "index.jsp";
+                }
+            } catch (SQLException e){
+                request.setAttribute("error", "Erreur SQL: Veuillez réessayer plus tard");
+                LOGGER.info("Erreur SQL: " + e.getMessage());
+                return "index.jsp";
+            }
+//        return "client/listeClient.jsp";
     }
 }
